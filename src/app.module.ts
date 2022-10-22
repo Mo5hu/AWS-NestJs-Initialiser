68c26627878f3f4cb84bb791b2489ea1a72dbab5
mohsin <k173634@nu.edu.pk>
2022-10-22 05:10:40 +0500

    first commit

---
+++ src/app.module.ts
@@ -0,0 +1,28 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { DynamooseModule } from 'nestjs-dynamoose';
+import { NotificationModule } from './notification/notification.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      playground: true,
+    }),
+    DynamooseModule.forRoot({
+      local: process.env.IS_DDB_LOCAL === 'true',
+      aws: { region: process.env.REGION },
+      table: {
+        create: process.env.IS_DDB_LOCAL === 'true',
+        prefix: `${process.env.SERVICE}-${process.env.STAGE}-`,
+        suffix: '-table',
+      },
+    }),
+    NotificationModule,
+  ],
+})
+export class AppModule {}
